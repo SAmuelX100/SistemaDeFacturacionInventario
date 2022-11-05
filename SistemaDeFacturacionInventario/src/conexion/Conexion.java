64b358ab8f471dd7312ca3bbf84f84221720e84f
SAmuelX100 <samuel118.sainthilaire@gmail.com>
2022-11-04 23:10:28 -0400

    Login Error Solucionado

--- SistemaDeFacturacionInventario/src/conexion/Conexion.java
+++ SistemaDeFacturacionInventario/src/conexion/Conexion.java
@@ -14,7 +14,7 @@ public class Conexion {
 
     public static Connection conectar() {
         try {
-            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_sistema_ventas", "root", "1234");
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_sistema_ventas", "root", "12345");
             return cn;
         } catch (SQLException e) {
             System.out.println("Error en la conexion local:");
